8e166a378f34a92f888e323fc7a37e772b2d226a
jasonhuang <jahhuang@ucsc.edu>
2020-12-02 16:03:46 -0800

    Some bug fixes

--- DFS_solution.py
+++ DFS_solution.py
@@ -1,8 +1,8 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.edges = ()
-        self.visited = False
+level3 = [[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,1,0,0]]
+level2 = [[0,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,0,0,1,0,0]]
+level1 = [[0,0,1],[0,0,0],[1,0,0]]
+
+
 #edges in a dictionary: each tile is a key, the value of each key is a list of ints for which other tiles are accessible
 def get_dimensions(level):
     rows = len(level)
@@ -38,21 +38,47 @@ def create_list_of_edges(level):
         elif tile >= 1 + (rows - 1) * columns:
             if tile % columns == 1:
                 edges[tile] = (tile - columns, tile + 1)
-            elif tile & columns == 0:
+            elif tile % columns == 0:
                 edges[tile] = (tile - columns, tile - 1)
             else:
                 edges[tile] = (tile - columns, tile - 1, tile + 1)
         else:
             if tile % columns == 1:
                 edges[tile] = (tile - columns, tile + 1, tile + columns)
-            elif tile & columns == 0:
+            elif tile % columns == 0:
                 edges[tile] = (tile - columns, tile - 1, tile + columns)
             else:
                 edges[tile] = (tile - columns, tile - 1, tile + 1, tile + columns)
+    for tile in boulders:
+        edges[tile] = ()
     return edges
 
 
+def solve_by_dfs(level, start, end):
+    edges = create_list_of_edges(level)
+    rows, columns = get_dimensions(level)
+    tiles, boulders = get_tile_types(level)
+    unvisited = tiles.copy()
+    solution = []
+    return traverse(unvisited,start,end,[],edges)
 
-level3 = [[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,1,0,0]]
-level2 = [[0,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,0,0,1,0,0]]
-level1 = [[0,0,1],[0,0,0],[1,0,0]]
+
+#board updates per recursive call, so maybe have each call generate its own copy of board to modify and path update
+# so if it's not a solution, we don't have to backtrack and undo any changes made 
+def traverse(unvisited, start, end, path, edges):
+    copy_unvisited = unvisited.copy()
+    copy_path = path.copy()
+    current = start
+    copy_unvisited.remove(current)
+    copy_path.append(current)
+    print(copy_path)
+    if current == end:
+        if len(copy_unvisited) == 0:
+            return copy_path
+    for i in edges[current]:
+        if i in copy_unvisited:
+            traverse(copy_unvisited, i, end, copy_path, edges)
+    
+edges = create_list_of_edges(level1)
+traverse([1,2,4,5,6,8,9], 8, 2, [], edges)
+
