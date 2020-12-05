21eec75f678591f0bc8eea03d62d01f16dee8c1a
jasonhuang <jahhuang@ucsc.edu>
2020-12-05 11:16:37 -0800

    Added timer comparison

--- DFS_solution.py
+++ DFS_solution.py
@@ -1,3 +1,5 @@
+import timeit
+
 level3 = [[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,1,0,0]]
 level2 = [[0,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,0,0,1,0,0]]
 level1 = [[0,0,1],[0,0,0],[1,0,0]]
@@ -58,28 +60,6 @@ def create_list_of_edges(level):
         edges[tile] = ()
     return edges
 
-
-def solve_by_dfs(level, start, end):
-    edges = create_list_of_edges(level)
-    rows, columns = get_dimensions(level)
-    tiles, boulders = get_tile_types(level)
-    unvisited = tiles.copy()
-    solution = []
-    return traverse(unvisited,start,end,[],edges)
-
-
-#board updates per recursive call, so maybe have each call generate its own copy of board to modify and path update
-# so if it's not a solution, we don't have to backtrack and undo any changes made 
-
-#Need to fix: prints out correctly, but returns None.
-
-#unvisited is an array of positive ints, e.g. [1,3,4,5]
-#start is an int in unvisited, e.g. 4
-#end is an int in unvisited, e.g. 5
-#edges is a dictionary, each key is an int in unvisited, each value is a list of connected nodes
-#edges looks like {1: (4,5), 3: (1), 4: (1,3,5), 5: (1,4)}
-#Goal is to use DFS to go from start to end while visiting all unvisited nodes, and return the path
-#e.g. 4 -> 3 -> 1 -> 5 
 def traverse(unvisited, start, end, path, edges,sol):
     copy_unvisited = unvisited.copy()
     copy_path = path.copy()
@@ -95,16 +75,107 @@ def traverse(unvisited, start, end, path, edges,sol):
             traverse(copy_unvisited, i, end, copy_path, edges,sol)
     return sol
 
+def solve_by_dfs(level, start, end):
+    edges = create_list_of_edges(level)
+    rows, columns = get_dimensions(level)
+    tiles, boulders = get_tile_types(level)
+    unvisited = tiles.copy()
+    solution = []
+    return traverse(unvisited,start,end,[],edges, solution)
 
-edges1 = create_list_of_edges(level1)
-print(traverse([1,2,4,5,6,8,9], 8, 2, [], edges1, []))
-edges2 = create_list_of_edges(level2)
-tiles,boulders = get_tile_types(level2)
-print(traverse(tiles,18,4,[],edges2,[]))
-edges3 = create_list_of_edges(level3)
-tiles2, boulders2 = get_tile_types(level3)
-print(traverse(tiles2,39,6,[],edges3,[]))
-edges4 = create_list_of_edges(level4)
-tiles4, boulders4 = get_tile_types(level4)
-print(traverse(tiles4,39,6,[],edges4,[]))
+
+print(timeit.timeit(
+    """level3 = [[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,1,0,0]]
+level2 = [[0,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,0,0,1,0,0]]
+level1 = [[0,0,1],[0,0,0],[1,0,0]]
+level4 = [
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]]
+
+
+#edges in a dictionary: each tile is a key, the value of each key is a list of ints for which other tiles are accessible
+def get_dimensions(level):
+    rows = len(level)
+    columns = len(level[0])
+    return rows, columns
+
+def get_tile_types(level):
+    tiles = []
+    boulders = []
+    tile_counter = 1
+    for a in level:
+        for i in a:
+            if i == 0:
+                tiles.append(tile_counter)
+            if i == 1:
+                boulders.append(tile_counter)
+            tile_counter += 1
+    return tiles, boulders
+
+
+def create_list_of_edges(level):
+    rows, columns = get_dimensions(level)
+    tiles, boulders = get_tile_types(level)
+    edges = {}
+    for tile in tiles:
+        if tile <= columns:
+            if tile % columns == 1:
+                edges[tile] = (tile + 1, tile + columns)
+            elif tile % columns == 0:
+                edges[tile] = (tile - 1, tile + columns)
+            else:
+                edges[tile] = (tile - 1, tile + 1, tile + columns)
+        elif tile >= 1 + (rows - 1) * columns:
+            if tile % columns == 1:
+                edges[tile] = (tile - columns, tile + 1)
+            elif tile % columns == 0:
+                edges[tile] = (tile - columns, tile - 1)
+            else:
+                edges[tile] = (tile - columns, tile - 1, tile + 1)
+        else:
+            if tile % columns == 1:
+                edges[tile] = (tile - columns, tile + 1, tile + columns)
+            elif tile % columns == 0:
+                edges[tile] = (tile - columns, tile - 1, tile + columns)
+            else:
+                edges[tile] = (tile - columns, tile - 1, tile + 1, tile + columns)
+    for tile in boulders:
+        edges[tile] = ()
+    return edges
+
+def traverse(unvisited, start, end, path, edges,sol):
+    copy_unvisited = unvisited.copy()
+    copy_path = path.copy()
+    current = start
+    copy_unvisited.remove(current)
+    copy_path.append(current)
+    if current == end and len(copy_unvisited)==0:
+        if len(sol) == 0:
+            for i in copy_path:
+                sol.append(i)
+    for i in edges[current]:
+        if i in copy_unvisited:
+            traverse(copy_unvisited, i, end, copy_path, edges,sol)
+    return sol
+
+def solve_by_dfs(level, start, end):
+    edges = create_list_of_edges(level)
+    rows, columns = get_dimensions(level)
+    tiles, boulders = get_tile_types(level)
+    unvisited = tiles.copy()
+    solution = []
+    return traverse(unvisited,start,end,[],edges, solution)
+
+
+
+solve_by_dfs(level3,39,6)
+
+
+
+""",number=1
+))
+
+
 
--- puzzle_solver.py
+++ puzzle_solver.py
@@ -1,5 +1,15 @@
+import timeit
 from pysat.solvers import Glucose3
 
+
+level3 = [[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,1,0,0]]
+level2 = [[0,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,0,0,1,0,0]]
+level1 = [[0,0,1],[0,0,0],[1,0,0]]
+level4 = [
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]]
 #takes input of [[],[],[],...] where each smaller array is the row, starting from the top of the level. Each smaller [] is a sequence
 # of 0s and 1s, where 1 is a boulder and 0 is an unused step.
 
@@ -131,26 +141,153 @@ def solve_puzzle(level, starting_tile, ending_tile):
                     i -= blocks
                 solutions.append(i)
         return solutions
-        
+
+
+print(timeit.timeit(
+    """from pysat.solvers import Glucose3
+
+
 level3 = [[0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,1,0,0]]
 level2 = [[0,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,0,0,1,0,0]]
 level1 = [[0,0,1],[0,0,0],[1,0,0]]
-#print(get_tile_types(level))
-#don't need to put 0 at the end of each clause, but also generates unused variables 1-10 (since i'm starting with 11)
-#How to number variables??? If there are x tiles, then 1,2,...,x = tile on step 1, x+1,...,2x = tile on step 
-# g = Glucose3()
-# g.add_clause([1,2])
-# g.add_clause([-2,3])
-# print((g.solve()))
-# print(type(g.solve()))
-# print(g.get_model())
-# print(type(g.get_model()[0]))
+level4 = [
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]]
+#takes input of [[],[],[],...] where each smaller array is the row, starting from the top of the level. Each smaller [] is a sequence
+# of 0s and 1s, where 1 is a boulder and 0 is an unused step.
 
-#test for the 3x3 puzzle (level 1):
-#print(solve_puzzle(level2, 8, 2))
+#takes a level encoding and returns the number of rows and columns. Assumes that the level is a rectangle and is encoded properly 
+def get_dimensions(level):
+    rows = len(level)
+    columns = len(level[0])
+    return rows, columns
 
-#test for the 3x7 puzzle (level 2):
-#print(solve_puzzle(level2,18,4))
+#takes a level encoding and returns array of tiles and array of boulders, numbered from  top left to bottom right
+def get_tile_types(level):
+    tiles = []
+    boulders = []
+    tile_counter = 1
+    for a in level:
+        for i in a:
+            if i == 0:
+                tiles.append(tile_counter)
+            if i == 1:
+                boulders.append(tile_counter)
+            tile_counter += 1
+    return tiles, boulders
 
-#test for the 4x11 puzzle (level 3):
-#print(solve_puzzle(level3,39,6))
+#takes a level, starting tile #, and ending tile # and returns a large array of CNF clauses encoding the level and puzzle rules
+def create_cnf_rules(level, starting_tile, ending_tile):
+    rows, columns = get_dimensions(level)
+    tiles, boulders = get_tile_types(level)
+    num_of_blocks = rows * columns
+    num_of_tiles = len(tiles)
+    #clauses is an array of arrays
+    clauses = []
+
+    #Must start at this tile:
+    for i in tiles:
+        if i == starting_tile:
+            clauses.append([i])
+        else:
+            clauses.append([-i])
+
+    #Must end at this tile:
+    for i in tiles:
+        if i == ending_tile:
+            clauses.append([i + (num_of_tiles-1)*num_of_blocks])
+        else:
+            clauses.append([-(i + (num_of_tiles-1)*num_of_blocks)])
+
+    #Boulders:
+    for i in boulders:
+        for j in range(num_of_tiles):
+            clauses.append([-(i + (num_of_blocks * j))])
+
+    #At least one tile per step:
+    for j in range(1, num_of_tiles):
+        clauses.append([i + j*num_of_blocks for i in tiles])
+
+    #At most one tile per step:
+    for i in tiles:
+        for j in tiles:
+            if j > i:
+                for k in range(num_of_tiles):
+                    clauses.append([-(i+num_of_blocks*k),-(j+num_of_blocks*k)])
+
+    #Each tile is used at least once by the end of the path:
+    for i in tiles:
+        dummy = []
+        for j in range(num_of_tiles):
+            dummy.append(i + num_of_blocks * j)
+        clauses.append(dummy)
+
+    #Each tile is used at most once by the end of the path:
+    for i in tiles:
+        dummy = [i + num_of_blocks * j for j in range(num_of_tiles)]
+        for j in dummy:
+            for k in dummy:
+                if k > j:
+                    clauses.append([-j,-k])
+    
+    #Path must be continuous (no skipping tiles): This works for only the first step, now need to extend to further steps. 
+    for i in tiles:
+        if i <= columns:
+            if i % columns == 1:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i + k*num_of_blocks), i + k*num_of_blocks + 1 + num_of_blocks, i+k*num_of_blocks + columns + num_of_blocks])
+            elif i % columns == 0:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks),i+k*num_of_blocks - 1 + num_of_blocks, i+k*num_of_blocks + columns + num_of_blocks])
+            else:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks), i +k*num_of_blocks + 1 + num_of_blocks, i +k*num_of_blocks - 1 + num_of_blocks, i +k*num_of_blocks + columns + num_of_blocks])
+        elif i >= 1 + (rows-1)*columns:
+            if i % columns == 1:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks), i+k*num_of_blocks + 1 + num_of_blocks, i+k*num_of_blocks - columns + num_of_blocks])
+            elif i % columns == 0:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks), i+k*num_of_blocks - 1 + num_of_blocks, i+k*num_of_blocks - columns + num_of_blocks])
+            else:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks), i+k*num_of_blocks - 1 + num_of_blocks, i+k*num_of_blocks + 1 + num_of_blocks, i+k*num_of_blocks - columns + num_of_blocks])
+        else:
+            if i % columns == 1:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks), i+k*num_of_blocks - columns + num_of_blocks, i+k*num_of_blocks + 1 + num_of_blocks, i+k*num_of_blocks + columns + num_of_blocks])
+            elif i % columns == 0:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks), i+k*num_of_blocks - columns + num_of_blocks, i+k*num_of_blocks - 1 + num_of_blocks, i+k*num_of_blocks + columns + num_of_blocks])
+            else:
+                for k in range(num_of_tiles-1):
+                    clauses.append([-(i+k*num_of_blocks), i+k*num_of_blocks - columns + num_of_blocks, i+k*num_of_blocks + 1 + num_of_blocks, i+k*num_of_blocks - 1 + num_of_blocks, i+k*num_of_blocks + columns + num_of_blocks])
+    return clauses
+    
+
+
+def solve_puzzle(level, starting_tile, ending_tile):
+    rows, columns = get_dimensions(level)
+    blocks = rows * columns
+    clauses = create_cnf_rules(level, starting_tile, ending_tile)
+    solver = Glucose3()
+    for i in clauses:
+        solver.add_clause(i)
+    if solver.solve() == False:
+        print("No solution!")
+        return
+    else:
+        solutions = []
+        for i in solver.get_model():
+            if i > 0:
+                while i > blocks:
+                    i -= blocks
+                solutions.append(i)
+        return solutions
+
+solve_puzzle(level3,39,6)""", 
+        number=1
+))
+
